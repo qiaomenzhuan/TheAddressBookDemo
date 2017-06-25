6e90a264e0d6963e0a439748e8d7886a1ac41469
qiaomenzhuan <1308332927@qq.com>
2017-06-25 12:14:29 +0800

    Delete YLPinYin.h

--- TheAddressBookDemo/YLPinYin.h
+++
@@ -1,9 +0,0 @@
-//
-//  YLPinYin.h
-//  TheAddressBookDemo
-//
-//  Created by YangLei on 16/3/15.
-//  Copyright © 2016年 YangLei. All rights reserved.
-//
-
-char pinyinFirstLetter(unsigned short hanzi);
